b503d26707f876b9c7369d9ac82238fe3ac06ac3
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-03-14 16:12:57 +0200

    Update تاك.py

--- sbb_b/plugins/تاك.py
+++ sbb_b/plugins/تاك.py
@@ -11,7 +11,7 @@ async def iq(sbb_b):
     mentions = sbb_b.text[8:]
     chat = await sbb_b.get_input_chat()
     async for x in sbb_b.client.iter_participants(chat, 200):
-        mentions += f" \n🝳 ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉"
+        mentions += f" \n◉  ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉"
     await sbb_b.client.send_message(sbb_b.chat_id, mentions)
     await sbb_b.delete()
 @sbb_b.on(admin_cmd(pattern="تاك 150(?: |$)(.*)"))
@@ -19,7 +19,7 @@ async def iq(sbb_b):
     mentions = sbb_b.text[8:]
     chat = await sbb_b.get_input_chat()
     async for x in sbb_b.client.iter_participants(chat, 150):
-        mentions += f" \n🝳 ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
+        mentions += f" \n◉  ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
     await sbb_b.client.send_message(sbb_b.chat_id, mentions)
     await sbb_b.delete()
 @sbb_b.on(admin_cmd(pattern="تاك 100(?: |$)(.*)"))
@@ -27,7 +27,7 @@ async def iq(sbb_b):
     mentions = sbb_b.text[8:]
     chat = await sbb_b.get_input_chat()
     async for x in sbb_b.client.iter_participants(chat, 100):
-        mentions += f" \n🝳 ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
+        mentions += f" \n◉  ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
     await sbb_b.client.send_message(sbb_b.chat_id, mentions)
     await sbb_b.delete()
 @sbb_b.on(admin_cmd(pattern="تاك 50(?: |$)(.*)"))
@@ -35,7 +35,7 @@ async def iq(sbb_b):
     mentions = sbb_b.text[8:]
     chat = await sbb_b.get_input_chat()
     async for x in sbb_b.client.iter_participants(chat, 50):
-        mentions += f" \n🝳 ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
+        mentions += f" \n◉  ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
     await sbb_b.client.send_message(sbb_b.chat_id, mentions)
     await sbb_b.delete()
 @sbb_b.on(admin_cmd(pattern="تاك 10(?: |$)(.*)"))
@@ -43,6 +43,6 @@ async def iq(sbb_b):
     mentions = sbb_b.text[8:]
     chat = await sbb_b.get_input_chat()
     async for x in sbb_b.client.iter_participants(chat, 10):
-        mentions += f" \n 🝳 ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
+        mentions += f" \n◉  ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉 \n"
     await sbb_b.client.send_message(sbb_b.chat_id, mentions)
     await sbb_b.delete()
