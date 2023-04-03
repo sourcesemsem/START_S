ba129306a7781cb3c3cde49219f179f44247a2d2
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-03 16:24:19 +0200

    Update تاك.py

--- sbb_b/plugins/تاك.py
+++ sbb_b/plugins/تاك.py
@@ -14,6 +14,22 @@ async def iq(sbb_b):
         mentions += f" \n◉  ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉"
     await sbb_b.client.send_message(sbb_b.chat_id, mentions)
     await sbb_b.delete()
+from telethon import functions
+from telethon.tl import functions
+from telethon.tl.functions.channels import InviteToChannelRequest
+
+from sbb_b import sbb_b
+
+from ..core.managers import edit_delete, edit_or_reply
+
+@sbb_b.on(admin_cmd(pattern="تاك(?: |$)(.*)"))
+async def iq(sbb_b):
+    mentions = sbb_b.text[8:]
+    chat = await sbb_b.get_input_chat()
+    async for x in sbb_b.client.iter_participants(chat, 200):
+        mentions += f" \n◉  ⦙ ⵧ〈[{x.first_name}](tg://user?id={x.id})〉"
+    await sbb_b.client.send_message(sbb_b.chat_id, mentions)
+    await sbb_b.delete()
 @sbb_b.on(admin_cmd(pattern="تاك 150(?: |$)(.*)"))
 async def iq(sbb_b):
     mentions = sbb_b.text[8:]
