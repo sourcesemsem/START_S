d995bc1b1f358f9ea2e2362d6eb85b2a5953169c
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-07 23:27:57 +0200

    Update startup.py

--- sbb_b/utils/startup.py
+++ sbb_b/utils/startup.py
@@ -148,12 +148,11 @@ async def startupmessage():
     else:
         try:
             if BOTLOG:
-                await sbb_b.tgbot.send_message(
-                    BOTLOG_CHATID,
-                    "**لقد تم بنجاح تنصيب سورس سيمو **\n➖➖➖➖➖➖➖➖➖➖\n**السورس**: @FTTUTY\n**المطور**: @DEV_SAMIR\n➖➖➖➖➖➖➖➖➖➖\n**مجموعة الدعم**: @FTTUTT0\n➖➖➖➖➖➖➖➖➖➖",
-                    buttons=[
-                        (Button.url("المبرمجہ سمير", "https://t.me/DEV_SAMIR"),)
-                    ],
+                await sbb_b.tgbot.send_file(
+                BOTLOG_CHATID,
+                "https://telegra.ph/file/1d033934ba8ab84145760.jpg",
+                caption="᯽︙ بــوت سيمو يـعـمـل بـنـجـاح  \n\n᯽︙ ارسل .الاوامر لرؤية اوامر السورس\n\n᯽︙ تـحـيـاتـي الـمـبـرمـجہ سـمـيـر",
+                buttons=[(Button.url("الـمـبـرمـجہ سـمـيـر", "https://t.me/DEV_SAMIR"),)],
                 )
         except Exception as e:
             LOGS.error(e)
